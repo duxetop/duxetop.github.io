e2e9037524d8f77d7a7fc73cbf3ca78731648346
duxetop <84699310+duxetop@users.noreply.github.com>
2021-05-23 23:39:02 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+document.getElementById("btn").addEventListener("click", getApi)
+let request = false
+function getApi() {
+	request = true
+	console.log(request)
+	let foodName = document.getElementById("phrase").value;
+	let parameter = document.getElementById("fields").value;
+	let answrField = document.getElementById("responseApi");
+	
+    if (request == true) {
+		let settings = {
+			"async": true,
+			"crossDomain": true,
+			"url": "https://nutritionix-api.p.rapidapi.com/v1_1/search/" + foodName + "?fields=" + parameter,
+			"method": "GET",
+			"headers": {
+				"x-rapidapi-key": "1661ca73b7mshbf37585e5b7fe43p140a04jsnbf8755f744f7",
+				"x-rapidapi-host": "nutritionix-api.p.rapidapi.com"
+			}
+	
+		};
+	
+	
+		
+		$.ajax(settings).done(function (response) {
+			
+			answrField.innerHTML = "";
+			$.each(response.hits[0].fields.parameterGet, function (key, value) {
+				console.log(response.hits[0].fields.parameterGet);
+				$(answrField).append(key + ": " + value + '<br>');
+			})
+	
+	
+	
+	
+	
+			// console.log(response.hits[]);
+			// answrField.innerHTML = response; 
+		});
+	}
+	
+}
